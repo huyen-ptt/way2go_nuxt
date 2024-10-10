b411bc81f7f3a66447beb281e233fd604d47033a
x <phamhuyen19124@gmail.com>
2024-10-10 13:54:37 +0700

    khung html

---
+++ pages/travel-support.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="page-header">
+    <div class="container">
+      <h2 class="inner-title">News</h2>
+    </div>
+  </div>
+  <!-- End Page Header -->
+  <!-- List Blog -->
+  <div class="section-11">
+    <div class="container">
+      <div class="inner-wrap">
+        <div class="row justify-content-center">
+          <div class="col-xl-8 col-lg-9 col-md-11 col-sm-12 col-12 blog-container">
+            <div class="blog-item mb-20">
+              <a href="bai-viet-kiem-thu-content.html" title="BÀI VIẾT KIỂM THỬ CONTENT">
+                <div class="inner-box">
+                  <div class="inner-image">
+                    <img
+                      src="../way2gocms.hndedu.com/uploads/thumb/2024/09/27/m%c3%a3%20p%c3%ad%20l%c3%a8ng.jpg"
+                      alt="BÀI VIẾT KIỂM THỬ CONTENT"
+                    />
+                  </div>
+                  <div class="inner-info">
+                    <h2 class="inner-title">BÀI VIẾT KIỂM THỬ CONTENT</h2>
+                    <div class="inner-desc">
+                      <p>
+                        B&Agrave;I VIẾT KIỂM THỬ CONTENTB&Agrave;I VIẾT KIỂM THỬ
+                        CONTENTB&Agrave;I VIẾT KIỂM THỬ CONTENT
+                      </p>
+                      ;
+                    </div>
+                    <div class="inner-date">03/10/2024</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="blog-item mb-20">
+              <a
+                href="mid-autumn-festival-in-hanoi-over-100-years-ago-kn.html"
+                title="Mid-Autumn Festival in Hanoi Over 100 Years Ago- KN"
+              >
+                <div class="inner-box">
+                  <div class="inner-image">
+                    <img
+                      src="../way2gocms.hndedu.com/uploads/thumb/2024/09/09/1att.jpg"
+                      alt="Mid-Autumn Festival in Hanoi Over 100 Years Ago- KN"
+                    />
+                  </div>
+                  <div class="inner-info">
+                    <h2 class="inner-title">
+                      Mid-Autumn Festival in Hanoi Over 100 Years Ago- KN
+                    </h2>
+                    <div class="inner-desc">
+                      <p>
+                        <em><strong>Mid-Autumn Festival</strong> </em>has existed for a
+                        long time and has always been a major celebration eagerly
+                        anticipated by both children and adults. In the early 20th century
+                        in Hanoi, as soon as the 8th lunar month began, parents would take
+                        their children to H&agrave;ng Gai and H&agrave;ng M&atilde;
+                        streets to admire and buy toys.
+                      </p>
+
+                      <p>&nbsp;</p>
+                      ;
+                    </div>
+                    <div class="inner-date">09/09/2024</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="blog-item mb-20">
+              <a
+                href="pho-nam-dinh-hanoi-and-mi-quang-have-been-recognized-as-national-intangible-cultural-heritage.html"
+                title="Pho Nam Dinh, Pho Hanoi, and Quang Noodle have been recognized as National Intangible Cultural Heritage"
+              >
+                <div class="inner-box">
+                  <div class="inner-image">
+                    <img
+                      src="../way2gocms.hndedu.com/uploads/thumb/2024/08/14/1pho2.jpg"
+                      alt="Pho Nam Dinh, Pho Hanoi, and Quang Noodle have been recognized as National Intangible Cultural Heritage"
+                    />
+                  </div>
+                  <div class="inner-info">
+                    <h2 class="inner-title">
+                      Pho Nam Dinh, Pho Hanoi, and Quang Noodle have been recognized as
+                      National Intangible Cultural Heritage
+                    </h2>
+                    <div class="inner-desc">
+                      <p>
+                        <strong>The Ministry of Culture, Sports, and Tourism</strong>
+                        (MoCST) has recently announced that three renowned dishes,
+                        namely<em
+                          ><strong> Pho Nam Dinh</strong> (Phở Nam Định),
+                          <strong>Pho Hanoi</strong> (Phở H&agrave; Nội), and
+                          <b>Quang Noodle</b> (M&igrave; Quảng)</em
+                        >, have been recognized as national intangible cultural heritage.
+                        These dishes have historical, intellectual, and community
+                        connection values. The Ministry emphasized that these cultural
+                        heritages represent the local identity, showcase cultural
+                        diversity and human creativity, and have the potential for
+                        restoration and long-term sustainability.
+                      </p>
+                      ;
+                    </div>
+                    <div class="inner-date">14/08/2024</div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="row justify-content-center mt-40">
+          <div class="col-xl-4 col-lg-6 col-sm-8 col-12">
+            <a
+              href="javascript:void(0)"
+              class="button button-full button-load-more"
+              data-current="1"
+              data-size="3"
+              data-id="1259"
+            >
+              View all posts
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- List Blog -->
+
+  <br />
+  <!--Khoi Banner Bottom-->
+  <!-- Section 23 -->
+  <div class="section-23 lazy-bg">
+    <div class="container">
+      <div class="inner-section-1">
+        <div class="inner-content">
+          <h2 class="inner-title">Way2go Global Travel SIM</h2>
+          <div class="inner-desc">
+            <p style:font-size:21px="">
+              <em>Enrich your travel experience while staying connected</em><br />
+              &nbsp;
+            </p>
+          </div>
+        </div>
+        <div class="inner-image">
+          <img
+            class="lazy"
+            data-src="https://way2gocms.hndedu.com//uploads/2024/03/19/mobile-mock.png"
+            alt="Hình ảnh khối chân trang"
+          />
+        </div>
+      </div>
+      <div class="inner-section-2">
+        <div class="inner-content">
+          <div class="inner-title">Register your information and Activate your SIM</div>
+          <a
+            href="https://registration.way2go.vn/"
+            target="_blank"
+            class="button button-white button-full"
+            >ACTIVATE</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
